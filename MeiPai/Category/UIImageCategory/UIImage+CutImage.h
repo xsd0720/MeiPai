434881f067ceb40c5be79778fec369af94d6a6b5
王辉 <xsd0720@163.com>
2016-08-22 22:12:39 +0800

    111

---
+++ MeiPai/Category/UIImageCategory/UIImage+CutImage.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+CutImage.h
+//  demo
+//
+//  Created by wany on 15/2/11.
+//  Copyright (c) 2015年 wany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (CutImage)
+//传入png图片  返回 坐标
++ (CGPoint)processImage:(UIImage*)inImage;
+
+@end
